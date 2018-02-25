3bcf682fec285d62c192eab0cb1a7e648cc263f6
Cafebo85 <34165947+Cafebo85@users.noreply.github.com>
2018-02-25 19:28:57 +0100

    Update hundir.c
    Se añaden comentarios e instrucciones de juego.
    Definiciones de funciones al final del código de la función "main" y declaración de funciones antes de la misma.
    Definitivo?

--- hundir.c
+++ hundir.c
@@ -10,291 +10,17 @@
 #define DIFBARCOS 5
 #define TOTALBARCOS 10
 
-
-//Declaracion funcion para colocar barcos
-void colocarBarcos(int posi1, int posi2, int posj1, int posj2, char mat[DIM][DIM], int vbarcos[])
-{
-    int i, j, cont;
-    cont=0;
-    if(posi1==posi2)
-    {
-        i=posi1;
-        for(j=posj1; j<=posj2; j++)
-        {
-            mat[i][j]='O';
-            cont++;
-        }
-    }
-    else
-    {
-        j=posj1;
-        for(i=posi1; i<=posi2; i++)
-        {
-            mat[i][j]='O';
-            cont++;
-        }
-    }
-    vbarcos[cont]=vbarcos[cont]-1;
-}
-//Declaracion funcion para comprobar la disponibilidad de la posicion elegida para el barco.
-int espacioDispo(int posi1, int posi2, int posj1, int posj2, char mat[DIM][DIM])
-{
-    int i, j;
-
-    for(i=posi1; i<=posi2; i++)
-    {
-        for(j=posj1; j<=posj2; j++)
-        {
-            if(mat[i][j]!=' ')
-            {
-                return -1;
-            }
-        }
-    }
-    return 1;
-}
-//Declaracion funcion para traducir la letra del eje "y" en un numero para movernos por la matriz.
-int letraxnum(char letra)
-{
-    switch (tolower(letra))
-    {
-    case 'a':
-        return 1;
-    case 'b':
-        return 2;
-    case 'c':
-        return 3;
-    case 'd':
-        return 4;
-    case 'e':
-        return 5;
-    case 'f':
-        return 6;
-    case 'g':
-        return 7;
-    case 'h':
-        return 8;
-    case 'i':
-        return 9;
-    case 'j':
-        return 10;
-    default:
-        return -1;
-    }
-}
-
-//Declaracion funcion para dejar libres las casillas alrededor del barco. Las rellenaremos con "agua".
-//Llamamos a la funcion con la posicion inicial y final del barco junto con la matriz del jugador.
-void agua(int posi1, int posi2, int posj1, int posj2, char mat[DIM][DIM])
-{
-    int i,j;
-
-    for(i=(posi1-1); i<(posi2+2); i++)
-    {
-        if(i>0 && i<DIM)
-        {
-            for(j=(posj1-1); j<(posj2+2); j++)
-            {
-                if (j>0 && j<DIM)
-                {
-                    if(posi1==posi2)
-                    {
-                        if(i!=posi1 || j<posj1 || j>posj2)
-                        {
-                            mat[i][j]='~';
-                        }
-                    }
-                    else
-                    {
-                        if(j!=posj1 || i<posi1 || i>posi2)
-                        {
-                            mat[i][j]='~';
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
-
-//Declaracion funcion para comprobar que la posicion elegida para el barco esta dentro del tablero.
-
-int posicionEnTablero(int posi1, int posi2, int posj1, int posj2)
-{
-    if(posi1==-1||posi2==-1)
-    {
-        return -1;
-    }
-    if(posi1!=posi2 && posj1!=posj2)
-    {
-        return -2;
-    }
-    if(posj1<1||posj1>10||posj2<1||posj2>10)
-    {
-        return -1;
-    }
-    return 1;
-}
-
-
-//Declaracion funcion para la creacion de los tableros de juego en blanco.
-void crearTablero(char mat[DIM][DIM])
-{
-    int i,j;
-    mat[0][0]=' ';
-    mat[0][1]='1';
-    mat[0][2]='2';
-    mat[0][3]='3';
-    mat[0][4]='4';
-    mat[0][5]='5';
-    mat[0][6]='6';
-    mat[0][7]='7';
-    mat[0][8]='8';
-    mat[0][9]='9';
-    mat[0][10]='0';
-    mat[1][0]='A';
-    mat[2][0]='B';
-    mat[3][0]='C';
-    mat[4][0]='D';
-    mat[5][0]='E';
-    mat[6][0]='F';
-    mat[7][0]='G';
-    mat[8][0]='H';
-    mat[9][0]='I';
-    mat[10][0]='J';
-    for (i=1; i<DIM; i++)
-    {
-        for (j=1; j<DIM; j++)
-        {
-            mat[i][j]=' ';
-        }
-    }
-}
-void mostrar(char mat[DIM][DIM])
-{
-    int i, j;
-    for (i=0; i<DIM; i++)
-    {
-        printf("\t\t\t\t");
-        for (j=0; j<DIM; j++)
-        {
-            printf("%c  ", mat[i][j]);
-        }
-        printf("\n");
-    }
-}
-
-void rellenarAgua(char mat[DIM][DIM])
-{
-    int i, j;
-    for(i=1; i<DIM; i++)
-    {
-        for(j=1; j<DIM; j++)
-        {
-            if(mat[i][j]==' ')
-            {
-                mat[i][j]='~';
-            }
-        }
-    }
-}
-int comprobarBarco(int posi1, int posi2, int posj1, int posj2, int barcos[])
-{
-    int i, j, tamano;
-    if (posi1 == posi2)
-    {
-        tamano = posj2 - posj1 + 1;
-    }
-    else
-    {
-        tamano = posi2 - posi1 + 1;
-    }
-    if (tamano > 5)
-    {
-        return (-1);
-    }
-    else
-    {
-        if (barcos[tamano] != 0)
-        {
-            return (1);
-        }
-        else
-        {
-            return (-2);
-        }
-    }
-}
-int tocadoHundido(int posi, int posj, char mat[DIM][DIM])
-{
-    int i, j;
-    i=posi;
-    j=posj;
-    while(i>0&&mat[i][j]=='X')
-    {
-        i--;
-    }
-    if(i>0)
-    {
-        if(mat[i][j]=='O')
-        {
-            return 1;
-        }
-    }
-    i=posi;
-    while(i<(DIM-1)&&mat[i][j]=='X')
-    {
-        i++;
-    }
-    if(i<DIM)
-    {
-        if(mat[i][j]=='O')
-        {
-            return 1;
-        }
-    }
-    i=posi;
-    while(j>0&&mat[i][j]=='X')
-    {
-        j--;
-    }
-    if(j>0)
-    {
-        if(mat[i][j]=='O')
-        {
-            return 1;
-        }
-    }
-    j=posj;
-    while(j<(DIM-1)&&mat[i][j]=='X')
-    {
-        i++;
-    }
-    if(i<DIM)
-    {
-        if(mat[i][j]=='O')
-        {
-            return 1;
-        }
-    }
-    return 2;
-}
-int disparo(int posi, int posj, char mat1[DIM][DIM], char mat2[DIM][DIM])
-{
-    int x;
-    if(mat1[posi][posj]=='O')
-    {
-        mat1[posi][posj]='X';
-        mat2[posi][posj]='X';
-        x=tocadoHundido(posi, posj, mat1);
-    }
-    else
-    {
-        mat2[posi][posj]='~';
-        x=-1;
-    }
-    return x;
-}
+void colocarBarcos();//Declaracion funcion para colocar barcos
+int espacioDispo();//Declaracion funcion para comprobar la disponibilidad de la posicion elegida para el barco.
+int letraxnum(char letra);//Declaracion funcion para traducir la letra del eje "y" en un numero para movernos por la matriz.
+void agua();//Declaracion funcion para dejar libres las casillas alrededor del barco.
+int posicionEnTablero();//Declaracion funcion para comprobar que la posicion elegida para el barco esta dentro del tablero.
+void crearTablero();//Declaracion funcion para la creacion de los tableros de juego en blanco.
+void mostrar();//Declaracion funcion para mostrar tablero por pantalla.
+void rellenarAgua();//Declaracion funcion para rellenar resto del tablero con agua
+int comprobarBarco();//Declaracion funcion para comprobar disponibilidad del barco.
+int tocadoHundido();//Declaracion funcion para comprobar si el barco ha sido hundido o unicamente tocado.
+int disparo();//Definicion funcion que comprueba resultado del disparo.
 
 void main()
 {
@@ -309,6 +35,15 @@ void main()
     crearTablero(tablero2A);
     crearTablero(tablero1B);
     crearTablero(tablero2B);
+    printf("\t\t\t\t\tBIENVENIDO AL JUEGO HUNDIR LA FLOTA.\n");
+    printf("\t\t\t\t\t\tEtapa colocacion.\n");
+    printf("\t\t\t\t\t\tInstrucciones:\n");
+    printf("- Cada jugador colocara sus barcos por turno. Primero el jugador 1 y despues el jugador 2.\n");
+    printf("- Los barcos a colocar seran los siguientes:\n");
+    for (x = 1; x < DIFBARCOS; x++){
+	    	printf("\tTamano: %d casillas Cantidad: %d barcos\n", x, barcosJugador1[x]);
+	}
+	system("pause");
     printf("Inicio primer jugador.\n");
     while(totbarcosj1!=TOTALBARCOS)
     {
@@ -366,14 +101,14 @@ void main()
                     {
                         if(barcOk==-2)
                         {
-                            printf("No quedan barcos de ese tamaño a introducir.\n");
+                            printf("No quedan barcos de ese tamano a introducir.\n");
                         }
                         else
                         {
                             posOk=espacioDispo(posi1, posi2, posj1, posj2, tablero1A);
                             if(posOk==-1)
                             {
-                                printf("No se puede colocar el barco en esta posicion. (Recuerda que los barcos no pueden tocarse.)");
+                                printf("No se puede colocar el barco en esta posicion. (Recuerda que los barcos no pueden tocarse)\n");
                             }
                         }
                     }
@@ -391,7 +126,6 @@ void main()
         system("cls");
     }
     rellenarAgua(tablero1A);
-    mostrar(tablero1A);
 
     printf("Inicio segundo jugador.\n");
     while(totbarcosj2!=TOTALBARCOS)
@@ -450,14 +184,14 @@ void main()
                     {
                         if(barcOk==-2)
                         {
-                            printf("No quedan barcos de ese tamaño a introducir.\n");
+                            printf("No quedan barcos de ese tamano a introducir.\n");
                         }
                         else
                         {
                             posOk=espacioDispo(posi1, posi2, posj1, posj2, tablero2A);
                             if(posOk==-1)
                             {
-                                printf("No se puede colocar el barco en esta posicion. (Recuerda que los barcos no pueden tocarse.)");
+                                printf("No se puede colocar el barco en esta posicion. (Recuerda que los barcos no pueden tocarse)\n");
                             }
                         }
                     }
@@ -475,14 +209,20 @@ void main()
         system("cls");
     }
     rellenarAgua(tablero2A);
-    mostrar(tablero2A);
+    system("cls");
     aux=0;
+    printf("\t\t\t\t\t\tEtapa de juego.\n\n");
+    printf("\t\t\t\t\t\tInstrucciones:\n\n");
+    printf("- Los jugadores iran diparando por turnos sucesivamente. Primero el jugador 1 y despues el jugador 2.\n\n\n");
+    printf("\t\t\t\t\t\tQue gane el mejor. Suerte!\n");
+    system("pause");
     while(totbarcosj1!=0 && totbarcosj2!=0)
     {
         while(aux%2==0)
         {
             printf("Turno del jugador 1.\n");
             printf("Dispara!\n");
+            mostrar(tablero1B);
             scanf("%c%d", &letra, &posj1);
             posi1=letraxnum(letra);
             fflush(stdin);
@@ -513,6 +253,7 @@ void main()
         }
         while(aux%2==1)
         {
+            mostrar(tablero2B);
             printf("Turno del jugador 2.\n");
             printf("Dispara!\n");
             scanf("%c%d", &letra, &posj1);
@@ -552,5 +293,294 @@ void main()
     {
         printf("\t\t\t\tEl jugador 2 ha ganado! Felicidades!");
     }
+}
+
+//Definicion funcion para colocar barcos en tablero
+void colocarBarcos(int posi1, int posi2, int posj1, int posj2, char mat[DIM][DIM], int vbarcos[])
+{
+    int i, j, cont;
+    cont=0;
+    if(posi1==posi2)
+    {
+        i=posi1;
+        for(j=posj1; j<=posj2; j++)
+        {
+            mat[i][j]='O';
+            cont++;
+        }
+    }
+    else
+    {
+        j=posj1;
+        for(i=posi1; i<=posi2; i++)
+        {
+            mat[i][j]='O';
+            cont++;
+        }
+    }
+    vbarcos[cont]=vbarcos[cont]-1;
+}
+//Definicion funcion para comprobar la disponibilidad de la posicion elegida para el barco.
+int espacioDispo(int posi1, int posi2, int posj1, int posj2, char mat[DIM][DIM])
+{
+    int i, j;
+
+    for(i=posi1; i<=posi2; i++)
+    {
+        for(j=posj1; j<=posj2; j++)
+        {
+            if(mat[i][j]!=' ')
+            {
+                return -1;
+            }
+        }
+    }
+    return 1;
+}
+
+//Definicion funcion para traducir la letra del eje "y" en un numero para movernos por la matriz.
+int letraxnum(char letra)
+{
+    switch (tolower(letra))
+    {
+    case 'a':
+        return 1;
+    case 'b':
+        return 2;
+    case 'c':
+        return 3;
+    case 'd':
+        return 4;
+    case 'e':
+        return 5;
+    case 'f':
+        return 6;
+    case 'g':
+        return 7;
+    case 'h':
+        return 8;
+    case 'i':
+        return 9;
+    case 'j':
+        return 10;
+    default:
+        return -1;
+    }
+}
+
+//Definicion funcion para dejar libres las casillas alrededor del barco. Las rellenaremos con "agua".
+//Llamamos a la funcion con la posicion inicial y final del barco junto con la matriz del jugador.
+void agua(int posi1, int posi2, int posj1, int posj2, char mat[DIM][DIM])
+{
+    int i,j;
+
+    for(i=(posi1-1); i<(posi2+2); i++)
+    {
+        if(i>0 && i<DIM)
+        {
+            for(j=(posj1-1); j<(posj2+2); j++)
+            {
+                if (j>0 && j<DIM)
+                {
+                    if(posi1==posi2)
+                    {
+                        if(i!=posi1 || j<posj1 || j>posj2)
+                        {
+                            mat[i][j]='~';
+                        }
+                    }
+                    else
+                    {
+                        if(j!=posj1 || i<posi1 || i>posi2)
+                        {
+                            mat[i][j]='~';
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+//Definicion funcion para comprobar que la posicion elegida para el barco esta dentro del tablero.
+int posicionEnTablero(int posi1, int posi2, int posj1, int posj2)
+{
+    if(posi1==-1||posi2==-1)
+    {
+        return -1;
+    }
+    if(posi1!=posi2 && posj1!=posj2)
+    {
+        return -2;
+    }
+    if(posj1<1||posj1>10||posj2<1||posj2>10)
+    {
+        return -1;
+    }
+    return 1;
+}
+
+
+//Definicion funcion para la creacion de los tableros de juego en blanco.
+void crearTablero(char mat[DIM][DIM])
+{
+    int i,j;
+    mat[0][0]=' ';
+    mat[0][1]='1';
+    mat[0][2]='2';
+    mat[0][3]='3';
+    mat[0][4]='4';
+    mat[0][5]='5';
+    mat[0][6]='6';
+    mat[0][7]='7';
+    mat[0][8]='8';
+    mat[0][9]='9';
+    mat[0][10]='0';
+    mat[1][0]='A';
+    mat[2][0]='B';
+    mat[3][0]='C';
+    mat[4][0]='D';
+    mat[5][0]='E';
+    mat[6][0]='F';
+    mat[7][0]='G';
+    mat[8][0]='H';
+    mat[9][0]='I';
+    mat[10][0]='J';
+    for (i=1; i<DIM; i++)
+    {
+        for (j=1; j<DIM; j++)
+        {
+            mat[i][j]=' ';
+        }
+    }
+}
+//Definicion funcion para mostrar tablero por pantalla.
+void mostrar(char mat[DIM][DIM])
+{
+    int i, j;
+    for (i=0; i<DIM; i++)
+    {
+        printf("\t\t\t\t");
+        for (j=0; j<DIM; j++)
+        {
+            printf("%c  ", mat[i][j]);
+        }
+        printf("\n");
+    }
+}
+//Definicion funcion para rellenar resto del tablero con agua. Se llama a la funcion una vez colocados todos los barcos.
+void rellenarAgua(char mat[DIM][DIM])
+{
+    int i, j;
+    for(i=1; i<DIM; i++)
+    {
+        for(j=1; j<DIM; j++)
+        {
+            if(mat[i][j]==' ')
+            {
+                mat[i][j]='~';
+            }
+        }
+    }
+}
+//Definicion funcion para comprobar disponibilidad del barco.
+int comprobarBarco(int posi1, int posi2, int posj1, int posj2, int barcos[])
+{
+    int i, j, tamano;
+    if (posi1 == posi2)
+    {
+        tamano = posj2 - posj1 + 1;
+    }
+    else
+    {
+        tamano = posi2 - posi1 + 1;
+    }
+    if (tamano > 5)
+    {
+        return (-1);
+    }
+    else
+    {
+        if (barcos[tamano] != 0)
+        {
+            return (1);
+        }
+        else
+        {
+            return (-2);
+        }
+    }
+}
+//Definicion funcion para comprobar si el barco ha sido hundido o unicamente tocado.
+int tocadoHundido(int posi, int posj, char mat[DIM][DIM])
+{
+    int i, j;
+    i=posi;
+    j=posj;
+    while(i>0&&mat[i][j]=='X')
+    {
+        i--;
+    }
+    if(i>0)
+    {
+        if(mat[i][j]=='O')
+        {
+            return 1;
+        }
+    }
+    i=posi;
+    while(i<(DIM-1)&&mat[i][j]=='X')
+    {
+        i++;
+    }
+    if(i<DIM)
+    {
+        if(mat[i][j]=='O')
+        {
+            return 1;
+        }
+    }
+    i=posi;
+    while(j>0&&mat[i][j]=='X')
+    {
+        j--;
+    }
+    if(j>0)
+    {
+        if(mat[i][j]=='O')
+        {
+            return 1;
+        }
+    }
+    j=posj;
+    while(j<(DIM-1)&&mat[i][j]=='X')
+    {
+        i++;
+    }
+    if(i<DIM)
+    {
+        if(mat[i][j]=='O')
+        {
+            return 1;
+        }
+    }
+    return 2;
+}
+//Definicion funcion que comprueba resultado del disparo.
+int disparo(int posi, int posj, char mat1[DIM][DIM], char mat2[DIM][DIM])
+{
+    int x;
+    if(mat1[posi][posj]=='O')
+    {
+        mat1[posi][posj]='X';
+        mat2[posi][posj]='X';
+        x=tocadoHundido(posi, posj, mat1);
+    }
+    else
+    {
+        mat2[posi][posj]='~';
+        x=-1;
+    }
+    return x;
 }
 
